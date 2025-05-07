4307e81ad423a87f95c33fe33661eb662712f1c4
kodongwon <dw9706@gmail.com>
2025-05-07 18:03:52 +0900

    first commit

---
+++ src/main/java/org/dongwon/scheduleappapi/ScheduleAppApiApplication.java
@@ -0,0 +1,13 @@
+package org.dongwon.scheduleappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dongwon/scheduleappapi/ScheduleAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dongwon.scheduleappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
