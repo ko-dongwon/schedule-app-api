1657eabb928db06a024d602be7325fab187fc506
kodongwon <dw9706@gmail.com>
2025-05-07 19:50:31 +0900

    chore: MySQL 및 JDBC 설정
    - JDBC 의존성 추가.
    - database 초기 설정.
    - schedule.sql 파일 추가

---
+++ schedule.sql
@@ -0,0 +1,17 @@
+CREATE TABLE authors(
+    author_id BIGINT AUTO_INCREMENT PRIMARY KEY ,
+    author_name VARCHAR(20) NOT NULL ,
+    email VARCHAR(100) NOT NULL ,
+    created_date TIMESTAMP NOT NULL ,
+    updated_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE schedules (
+    schedule_id BIGINT AUTO_INCREMENT PRIMARY KEY ,
+    content VARCHAR(200) NOT NULL ,
+    password VARCHAR(20) NOT NULL ,
+    author_id BIGINT NOT NULL ,
+    CONSTRAINT fk_schdules_authors FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+
