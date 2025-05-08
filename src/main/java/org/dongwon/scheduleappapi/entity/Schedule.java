154b5a7f3893f0b6b222cd089c3e03d677b97372
kodongwon <dw9706@gmail.com>
2025-05-09 02:21:45 +0900

    feat: Schedule 정적 팩토리 메서드 추가
    - DB에서 조회한 데이터로 Schedule 객체를 재구성할 용도로 만듬
    - 생성자 추가

--- src/main/java/org/dongwon/scheduleappapi/entity/Schedule.java
+++ src/main/java/org/dongwon/scheduleappapi/entity/Schedule.java
@@ -14,14 +14,28 @@ public class Schedule {
     private Long authorId;
 
     private Schedule(String content, String password, LocalDateTime createdAt, LocalDateTime updatedAt, Long authorId) {
+        this(null, content, password, createdAt, updatedAt, authorId);
+    }
+
+    private Schedule(Long id, String content, String password, LocalDateTime createdAt, LocalDateTime updatedAt, Long authorId) {
+        this.id = id;
         this.content = content;
         this.password = password;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.authorId = authorId;
     }
-
+    /*
+    * DB에 저장하기 전, 새로운 Schedule 객체를 생성
+    * createdAt과 updatedAt은 호출 시점의 현재 시간으로 설정되며, id는 할당되지 않습니다.
+    * */
     public static Schedule createSchedule(String content, String password, Long authorId) {
         return new Schedule(content, password, LocalDateTime.now(), LocalDateTime.now(), authorId);
+    }
+    /*
+    * DB에서 조회된 데이터로 Schedule 객체를 재구성
+    * */
+    public static Schedule createSchedule(Long id, String content, String password, Long authorId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        return new Schedule(id, content, password, createdAt, updatedAt, authorId);
     }
 }
